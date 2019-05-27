220517e236197bb9ff7126773fe3be1532e73a9e
i-chan <iwashiro2000@gmail.com>
2019-05-27 16:14:39 +0900

    [finish]kadai2

---
+++ vagrant/kadai/app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  end
+
+  def new
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+   book = Book.new(book_params)
+   if book.save
+    flash[:notice] = "successfully create book"
+    redirect_to book_path(book.id)
+   end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  	flash[:notice] = "successfully update book"
+  	redirect_to book_path(book.id)
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  private
+   def book_params
+     params.require(:book).permit(:title, :body)
+   end
+end
